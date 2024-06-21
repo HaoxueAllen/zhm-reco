b5011e06b4dc2beb465af7fed7d58188798c5ac3
HaoxueAllen <haoxueyuecqu1@gmail.com>
2024-06-21 08:08:02 +0800

    waline

--- .vuepress/configs/waline.ts
+++ .vuepress/configs/waline.ts
@@ -1,5 +1,9 @@
 export const commentConfig = {
-        serverURL: 'https://zhm-reco.vercel.app/',
-        
-        hideComments: false,
-}
+	type: "waline",
+	// options 与 1.x 的 valineConfig 配置一致
+	options: {
+		serverURL: "https://zhm-reco.vercel.app/",
+
+		hideComments: false,
+	},
+};
